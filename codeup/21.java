d3ad31d9fdfa8666672d669e29a015056176a9b8
h0mil <>
2022-08-02 04:42:10 +0000

    단어 1개 입력받아 그대로 출력하기

---
+++ codeup/21.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{
+  public static void main(String[] args){
+    String data= "";
+    Scanner sc = new Scanner(System.in);
+    data=sc.next();
+    sc.close();
+    System.out.println(data);
+    
+  }
+}
