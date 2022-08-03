6d9eba521efa8833f4d888ac10c4f7ee6de1d9f8
h0mil <>
2022-08-03 04:58:50 +0000

    년월일 입력 받아 형식 바꿔 출력하기

---
+++ codeup/26.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String[] h=sc.next().split(":");
+    System.out.println(h[1]);
+    
+    
+    
+  }
+}
