d2386fdaebdacedcca6797386c31bf7898255a9e
h0mil <>
2022-08-04 04:34:23 +0000

    28~37

---
+++ codeup/34.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    String b = sc.next();
+    int a;
+    a=Integer.parseInt(b,8);
+    System.out.printf("%d",a);
+    
+  }
+}
---
+++ codeup/35.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String b=sc.next();
+    int num = Integer.parseInt(b,16);
+    System.out.printf("%o", num);
+
+
+
+
+    
+  }
+}
