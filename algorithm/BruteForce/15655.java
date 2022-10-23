fae424968a1500e1401259b9e3e7d6f52a3609d0
h0mil <>
2022-10-23 13:30:00 +0000

    백트래킹 오름차순

---
+++ algorithm/BruteForce/15655.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main{
+  public static int n,m;
+  public static int[] graph;
+  public static int[] temp;
+  public static boolean[] visited;
+  public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  public static void dfs(int start, int depth)throws IOException{
+    if(depth==m){
+      for(int i=0;i<m;i++){
+        bw.write(graph[i]+" ");
+        
+      }
+      bw.newLine();
+        return;
+    }
+
+    for(int i=start;i<n;i++){
+      
+        
+        graph[depth] = temp[i];
+        dfs(i+1, depth+1);
+        
+      
+    }
+  }
+  
+  public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    
+    temp = new int[n];
+    graph = new int[m];
+    visited = new boolean[n];
+    
+    st = new StringTokenizer(br.readLine());
+    for(int i=0;i<n;i++){
+      temp[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(temp);
+
+    dfs(0, 0);
+    
+    bw.flush();
+    bw.close();
+  }
+}
